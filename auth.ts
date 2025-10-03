49519356443ca1509885d8b5e9f24af933ee4a87
TridibeshSam31 <tridisam651@gmail.com>
2025-10-04 01:28:13 +0530

    ok

--- auth.ts
+++ auth.ts
@@ -1,5 +1,30 @@
 import NextAuth from "next-auth"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks:{
+    async signIn(user,account){
+        if(!user||!account) return false
+
+        const existingUser = await db.user.findUnique({
+            where:{
+                email:user.email,
+            }
+        })
+
+        if(!existingUser){
+            //user aaya hi pehli baar hai 
+            const newUser = await db.user.create({
+                data:{
+                    
+                }
+
+            
+         })
+        }
+
+    },
+    async jwt(){},
+    async session(){},
   
+  }
 })
