807366f6e49f95601571db91b83ac182c1de9d93
TridibeshSam31 <tridisam651@gmail.com>
2025-10-08 00:39:10 +0530

    full dashboard ui done now logic remaining

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import AddNewButton from '@/modules/dashboard/components/add-new'
 import AddRepo from '@/modules/dashboard/components/add-new-repo'
+import {getAllPlaygroundForUser} from '@/modules/dashboard/actions'
+import ProjectTable from '@/modules/dashboard/components/project-table'
+import EmptyState from '@/modules/dashboard/components/empty-state'
 
 
 
 
-const page = () => {
+
+const page = async() => {
+    const playgrounds = await getAllPlaygroundForUser()
   return (
     <div className='flex flex-col justify-center items-center min-h-screen mx-auto max-w-7xl px-4 py-10'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-full'>
             <AddNewButton/>
             <AddRepo/>
+        </div>
+        <div className='mt-10 flex flex-col justify-center items-center w-full'>
+             {playgrounds && playgrounds.length === 0 ? (
+          <EmptyState />
+        ) : (
+          <ProjectTable
+            projects={playgrounds || []}
+            onDeleteProject={deleteProjectById}
+            onUpdateProject={editProjectById}
+            onDuplicateProject={duplicateProjectById}
+          />
+        )}
+
         </div>
     </div>
   )
