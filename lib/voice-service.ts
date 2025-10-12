d5fb6b360e2cd4d51f1db495a3653985bf0d5355
TridibeshSam31 <tridisam651@gmail.com>
2025-10-12 18:29:45 +0530

    future update

---
+++ lib/voice-service.ts
@@ -0,0 +1,2 @@
+//we will add voice commands features here in future after integrating ollama and monaco editor in it
+//in this we will also add some keyboard commands in order to activate voice mode coding 
