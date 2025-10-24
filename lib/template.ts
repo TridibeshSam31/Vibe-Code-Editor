5dd025d4ed5d59d328efea9f4eb62e5b265f1a79
TridibeshSam31 <tridisam651@gmail.com>
2025-10-24 15:52:45 +0530

    added starters file copied from stackblitz(bolt) and did file exploration path(added path to json) moving forward

---
+++ lib/template.ts
@@ -0,0 +1,12 @@
+export const templatePaths = {
+    REACT:"/vibecode-starters/react-ts",
+    NEXTJS:"/vibecode-starters/nextjs-new",
+    EXPRESS:"/vibecode-starters/express-simple",
+    VUE:"/vibecode-starters/vue",
+    HONO:"/vibecode-starters/hono-nodejs-starter",
+    ANGULAR:"/videcode-starters/angular"
+}
+
+
+//humne yahan par path define kr diye hai ki agar koi react templatte choose krega toh fir woh yeh vibecode-starters mai jayega aur udhar se react template start kr dega
+//now we wnat to convert this path into actual json files
