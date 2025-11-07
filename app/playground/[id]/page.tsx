108735bdc5c8f9c7e65a14de00e5d1860d5f3a18
TridibeshSam31 <tridisam651@gmail.com>
2025-11-07 23:54:22 +0530

    file structure page  for file exploration

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,4 +1,7 @@
 "use client"
+import { Separator } from '@/components/ui/separator';
+import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { usePlayground } from '@/modules/playground/hooks/usePlayground';
 import { useParams } from 'next/navigation'
 import React from 'react'
@@ -11,11 +14,42 @@ const MainPlaygroundPage = () => {
     //for checking purpose 
     console.log("templateData",templateData)
     console.log("playgroundData",playgroundData)
-
+  //ui for file exploration
   return (
-        <div>
-            params : {id}
-        </div>
+        <TooltipProvider>
+            <>
+            <TemplateFileTree
+            data={templateData}
+            onFileSelect={()=>{}}
+            selectedFile={()=>{}}
+            title="File Explorer"
+            onAddFile={()=>{}}
+            onAddFolder={()=>{}}
+            onDeleteFolder={()=>{}}
+            onRenameFile={()=>{}}
+            onRenameFolder={()=>{}}
+            
+            />
+            <SidebarInset>
+              <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
+                <SidebarTrigger className='-ml-1'/>
+                <Separator orientation='vertical' className='mr-2 h-4'/>
+
+              </header>
+              <div className='flex flex-1 items-center gap-2'>
+                <div className='flex flex-col flex-1'>
+                  <h1 className='text-sm font-medium '>
+                    {playgroundData?.title||"code Playground"}
+                  </h1>
+
+                </div>
+              </div>
+            </SidebarInset>
+
+
+
+            </>
+        </TooltipProvider>
   )
 }
 
--- modules/playground/hooks/usePlayground.tsx
+++ modules/playground/hooks/usePlayground.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner"
 
 interface PlaygroundData {
     id:string,
-    name?:string,
+    title?:string,
     [key:string]:any
 
 }
